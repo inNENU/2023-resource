e15c60e15c49a189e894e0c28cce18c1c5be41c4
Mr.Hope <mister-hope@outlook.com>
2023-06-09 14:28:24 +0800

    fix: fix location component

--- scripts/components/location/index.ts
+++ scripts/components/location/index.ts
@@ -42,15 +42,15 @@ ${
 `
     : ""
 }\
-<iframe src="https://apis.map.qq.com/tools/poimarker?type=0&marker=${points
+<iframe class="location-iframe" src="https://apis.map.qq.com/tools/poimarker?type=0&marker=${points
     // maximum 4 points
     .slice(0, 4)
     .map(
       ({ latitude, longitude, name = "", detail = "" }) =>
-        `coord:${latitude},${longitude};title:${name};addr:${detail}|`
+        `coord:${latitude},${longitude};title:${name};addr:${detail}`
     )
     .join(
-      "&"
+      "|"
     )}&key=YNUBZ-AN3HF-P62JK-J2GND-XQTQQ-TTBOB&referer=in东师" frameborder="0" width="100%" height="320px" />
 
 `;
